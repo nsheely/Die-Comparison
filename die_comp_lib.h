ee6d62ae94051689a1dd9e3b5cbb1cc47354004e
nsheely <40345092+nsheely@users.noreply.github.com>
2019-03-20 11:24:07 -0500

    Create die_comp_lib.h

---
+++ die_comp_lib.h
@@ -0,0 +1,12 @@
+/*
+ * Project: Die Comparison
+ * Author: Nathan Sheely
+ * File: die_comp_lib.h
+ *
+ * Purpose: Header file for die_comp_lib
+ *
+ * Contains:
+ *  void find_comb();
+ */
+ 
+ void find_comb(int n, int die_faces, int die[]);
